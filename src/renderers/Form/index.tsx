0df78142566dacdfd4a69328d9d410094d7ddab5
Hitheworld <cdxping@163.com>
2022-03-14 17:45:43 +0800

    init

--- src/renderers/Form/index.tsx
+++ src/renderers/Form/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useContext, useReducer } from 'react';
 import cx from 'classnames';
 import { Form } from 'antd';
 import { Renderer } from '@/factory';
+import LazyComponent from '@/components/LazyComponent';
 import { isVisible, bulkBindFunctions } from '@/utils/helper';
 import { RootStoreContext } from '@/store';
 
@@ -32,6 +33,44 @@ function FormRenderer(props: any) {
     console.log('Failed:', errorInfo);
   };
 
+  const renderBody = () => {
+    return (
+      <></>
+    )
+  }
+
+  let body: JSX.Element = renderBody();
+
+  if (wrapWithPanel) {
+    body = render(
+      'body',
+      {
+        type: 'panel',
+        title: title,
+      },
+      {
+        // className: cx(panelClassName, 'Panel--form'),
+        // children: body,
+        // actions: this.buildActions(),
+        // onAction: this.handleAction,
+        // onQuery: this.handleQuery,
+        // disabled: store.loading,
+        // btnDisabled: store.loading || store.validating,
+        // headerClassName,
+        // footer,
+        // footerClassName,
+        // footerWrapClassName,
+        // actionsClassName,
+        // bodyClassName,
+        // affixFooter,
+      }
+    ) as JSX.Element;
+  }
+
+  if (lazyLoad) {
+    body = <LazyComponent>{body}</LazyComponent>;
+  }
+
   return (
     <>
       这是表单
@@ -44,31 +83,7 @@ function FormRenderer(props: any) {
         onFinishFailed={onFinishFailed}
         autoComplete="off"
       >
-        {
-          render(
-            'body',
-            {
-              type: 'panel',
-              title: title,
-            },
-            {
-              // className: cx(panelClassName, 'Panel--form'),
-              // children: body,
-              // actions: this.buildActions(),
-              // onAction: this.handleAction,
-              // onQuery: this.handleQuery,
-              // disabled: store.loading,
-              // btnDisabled: store.loading || store.validating,
-              // headerClassName,
-              // footer,
-              // footerClassName,
-              // footerWrapClassName,
-              // actionsClassName,
-              // bodyClassName,
-              // affixFooter,
-            }
-          ) as JSX.Element
-        }
+        {body}
       </Form>
     </>
   );
